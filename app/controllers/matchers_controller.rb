a5435ff8b0019355ebe47d7b8309e8d0896d995f
Eric Artzt <artzte@baddabigboom.com>
2013-10-06 07:14:35 -0700

    Create method stub

--- app/controllers/matchers_controller.rb
+++ app/controllers/matchers_controller.rb
@@ -2,4 +2,25 @@ class MatchersController < ApplicationController
   def index
     render json: Matcher.all
   end
+
+  def create
+  	matcher = Matcher.new matcher_params
+    if matcher.save
+      matcher.run Transaction.unassigned
+      render json: matcher
+    else
+      render json: matcher, status: :bad_request
+    end
+  end
+
+  def update
+
+  end
+
+protected
+
+  def matcher_params
+    params.require(:matcher).permit(:category_id, :words)
+  end
+
 end

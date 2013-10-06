f8ed4366dacca6a0d8f81d07a693fbd0bfc56c53
Eric Artzt <artzte@baddabigboom.com>
2013-10-06 07:16:25 -0700

    Small matcher test

---
+++ test/models/matcher_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+describe Matcher do
+
+  setup do
+    @category = Category.create name: "Test"
+
+    @descriptions = [
+      "joes grocery store 102133",
+      "joes grocery store 100222",
+      "corner gas station 33322",
+      "janes hardware emporium west 39th",
+    ]
+
+    @descriptions.each do |t|
+      Transaction.create! description: t
+    end
+  end
+
+  it "matches full string" do
+    m = Matcher.create! words: "joes grocery store 102133", category: @category
+    m.run Transaction.all
+
+    CategoryTransaction.count.must_equal 1
+    CategoryTransaction.first.transaction.description.must_equal @descriptions.first
+  end
+
+  it "matches some words" do
+    m = Matcher.create! words: "joes 102133", category: @category
+    m.run Transaction.all
+
+    CategoryTransaction.count.must_equal 1
+    CategoryTransaction.first.transaction.description.must_equal @descriptions.first
+  end
+
+end
+

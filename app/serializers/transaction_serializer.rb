a911e42e60a95aa54acef2505216d6f29eccd0f0
Eric Artzt <artzte@baddabigboom.com>
2013-10-06 07:16:02 -0700

    Put matcher, category IDs in transaction json

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,13 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :_transaction_date, :_description, :_transtype, :_amount, :_transaction_id, :account, :posted_at, :description, :amount, :transaction_type, :matcher_id, :category_id
+  attributes :id, :account, :posted_at, :description, :amount, :transaction_type
+  attributes :matcher_id, :category_id
+
+  def matcher_id
+    object.category_transaction && object.category_transaction.matcher_id
+  end
+
+  def category_id
+    object.category_transaction && object.category_transaction.category_id
+  end
+
 end
